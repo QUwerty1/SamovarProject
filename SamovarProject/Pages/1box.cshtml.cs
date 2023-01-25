6228d42cf7494a214e0e9cc0c84ccbce464b372e
Никита <mrquwerty@outlook.com>
2023-01-25 08:45:05 +0800

    commit

---
+++ SamovarProject/Pages/1box.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SamovarProject.Pages
+{
+    public class _1boxModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
